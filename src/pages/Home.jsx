4ac72e1945e0a63233efffde81600386bf1140d6
pq8sim <pq.sim8@gmail.com>
2022-10-01 16:07:46 -0300

    API Working :)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,36 @@ import { View, Text } from "react-native"
 import api from '../api/api'
 
 export default function Home() {
-    const [imagens, setImagens] = useState([]);
-    const CLIENT_ID = "vFt3WJN6Xbre0Rgg_Byz_Pyvtm_ek653SoOmjDsDtwo";
+    const CLIENT_ID = "vFt3WJN6Xbre0Rgg_Byz_Pyvtm_ek653SoOmjDsDtwo"
+
+    const [imagens, setImagens] = useState([])
+    const [pais, setPais] = useState('')
 
     useEffect(() => {
-        api.get(`photos?client_id=${CLIENT_ID}`).then(response => {
-            // search/photos?query=brazil :c
-            setImagens(response.data);
+        api.get(`search/photos?query=${pais}&client_id=${CLIENT_ID}`).then(response => {
+            setImagens(response.data.results);
         })
     })
 
     return (
         <>
-            {imagens.map(img => (
-                <View key={img.id}>
-                    <Text>{img.id}</Text>
-                    <img src={img.urls.thumb} />
-                    <Text>{img.user.bio}</Text>
-                </View>
-            ))}
+            <form>
+                <input type="text"
+                    placeholder="Insira o Pais"
+                    values={pais}
+                    onChange={e => setPais(e.target.value)}
+                />
+                <button >Trocar pais</button>
+            </form>
+            {pais !== '' ?
+                imagens.map(img => (
+                    <View key={img.id}>
+                        <Text>{img.id}</Text>
+                        <img src={img.urls.thumb} />
+                        <Text>{img.user.bio}</Text>
+                    </View>
+                ))
+                : <Text>Pesquise por um Pais!</Text>}
         </>
     );
 }
